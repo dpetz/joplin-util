751d6b9b34ce4e4c9207bf982cf20319282ffe6d
dpetz <Dirk.Petzoldt@gmail.com>
2023-11-02 20:23:02 +0100

    DB and DIR works with argparse

--- resources.py
+++ resources.py
@@ -9,6 +9,8 @@ from os.path import isfile, join
 
 from collections import Counter
 
+import argparse
+
 with open('.token','r') as f:
     token = f.read().splitlines()[0] # avoid readline() which appends \n to each line
 
@@ -18,6 +20,7 @@ api = Api(token)
 # resource_id = api.add_resource(filename="path/to/image.png", title="My first resource")
 res_missed = "4df90691567d42efb93772717e9c56c7"
 res_exists = "8d48085ce54c4543bcd700fbe3de206c"
+res_data_example = """ResourceData(type_=None, id='467e9cc0983c4dd6bce42bcb9f967cc8', title='0f0a2d54285f6fda0da938248de92feb_6c6a8b96f9e94b5cb50ef983a6bc9c2c.jpg', mime=None, filename=None, created_time=None, updated_time=None, user_created_time=None, user_updated_time=None, file_extension=None, encryption_cipher_text=None, encryption_applied=None, encryption_blob_encrypted=None, size=None, is_shared=None, share_id=None, master_key_id=None)"""
 
 # Prints name
 # code = api.get_resource(res_exists)
@@ -43,20 +46,24 @@ def resource_directory_as_map(resource_directory="/Users/dirk/.config/joplin-des
 	for c in Counter(endings).most_common():
 		print(c[0],"\t",c[1])
 
+command = None
+commands = {
+	"db":"list all resources in database",
+	"dir": "list all resources in directory"
+}
 
-def example_resource_data():
-	return """
-ResourceData(type_=None, id='467e9cc0983c4dd6bce42bcb9f967cc8', title='0f0a2d54285f6fda0da938248de92feb_6c6a8b96f9e94b5cb50ef983a6bc9c2c.jpg', mime=None, filename=None, created_time=None, updated_time=None, user_created_time=None, user_updated_time=None, file_extension=None, encryption_cipher_text=None, encryption_applied=None, encryption_blob_encrypted=None, size=None, is_shared=None, share_id=None, master_key_id=None)
-"""
+def parse_arguments():
+	commands = { "db":"list all in database", "dir": "list all in directory" }
+	commands_help = "Supported Commands:\n" + "\n".join( k + "\t" + v for k, v in commands.items() )
+	argp = argparse.ArgumentParser(description='Explore and modify Joplin resources',
+		epilog=commands_help, formatter_class=argparse.RawDescriptionHelpFormatter)
+	argp.add_argument("command", choices=commands.keys())
+	return argp.parse_args(sys.argv[1:])
 
-# TODO argparse https://stackoverflow.com/questions/4033723/how-do-i-access-command-line-arguments
+args = parse_arguments()
 
-
-if (len(sys.argv) != 2):
-	print("Usage: python3 resources.py [command]")
-elif (sys.argv[1] == "db"):
-	print(*all(), sep = '\n')
-elif (sys.argv[1] == "dir"):
-	resource_directory_as_map()
-else:
-	print("Supported Commands:\n db\tlist all in database\n dir\tlist all in resource directory")
+match args.command:
+	case "db":
+		print(*all(), sep = '\n')
+	case "dir":
+		resource_directory_as_map()
